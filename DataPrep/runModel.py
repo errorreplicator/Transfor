e0a5a42506a973a8843aeff9afcc46e0b9d69300
Piotr JJ <error.replicator@gmail.com>
2022-06-05 22:39:11 +0200

    MEMORY FAILURES - one hot embedd

--- DataPrep/runModel.py
+++ DataPrep/runModel.py
@@ -1,6 +1,6 @@
 import pickle
 from pathlib import Path
-
+import numpy as np
 from keras.models import Model
 from keras.layers import Input,Dense, concatenate
 from keras.utils import plot_model
@@ -19,38 +19,69 @@ with open(mypath/'X_Y_index_pairs.pklxy','rb') as file:
 with open(mypath/'tokenizer.pkltok','rb') as file:
     tokenizer = pickle.load(file)
 
-CORP_SIZE = len(tokenizer.index_word)
-EMBED_SIZE = 100
+CORP_SIZE = 96000 #len(tokenizer.index_word)
+EMBED_SIZE = 50
+
+X_word = np.zeros((CORP_SIZE, CORP_SIZE))
 
 
-input_word = Input(shape=(CORP_SIZE,),name="word_layer_input")
-input_context = Input(shape=(CORP_SIZE,),name="context_layer_input")
+#*****************************************
+# print(10*" START ")
+# X_word = tf.one_hot(X_train[:,[0]].reshape(len(X_train),),CORP_SIZE)
+# X_context = tf.one_hot(X_train[:,[1]].reshape(len(X_train),),CORP_SIZE)
+# print(10*" STOP ")
 
-word_layer = Dense(EMBED_SIZE, activation='linear', name="word_lay_den_lin")(input_word)
-word_layer = Model(inputs = input_word, outputs=word_layer, name="word_layer_Model")
 
-context_layer = Dense(EMBED_SIZE, activation='linear', name="context_lay_den_lin")(input_context)
-context_layer = Model(inputs = input_context, outputs=context_layer,name='context_layer_Model')
+#*****************************************
+# print(10*" START ")
+#
+# word_sample_onehot_list = X_train[:, [0]].reshape(len(X_train), )
+# context_sample_onehot_list = X_train[:, [1]].reshape(len(X_train), )
+# row_size = len(word_sample_onehot_list)
+#
+# X_word = np.zeros((row_size,CORP_SIZE))
+# X_context = np.zeros((row_size,CORP_SIZE))
+#
+# for index, int_number in enumerate(word_sample_onehot_list):
+#     X_word[index][int_number] = 1
+#
+# for index, int_number in enumerate(context_sample_onehot_list):
+#     X_context[index][int_number] = 1
+#
+# print(10*" STOP ")
+#*****************************************
 
-combined = concatenate([word_layer.output, context_layer.output], name="concatenate")
-output = Dense(1,activation='sigmoid',name="output_layer_1_sigmoid")(combined)
 
-model = Model(inputs = [word_layer.input,context_layer.input],outputs = output,name="final_model")
-
-opt = Adam(learning_rate=1e-3)
-
-model.compile(loss="binary_crossentropy",optimizer=opt,metrics=['accuracy'])
-
-model.fit(
-    x=[tf.one_hot(X_train[0],CORP_SIZE),tf.one_hot(X_train[1],CORP_SIZE)],
-    y=y_train,
-    epochs=200,
-    batch_size=128
-)
-
+# input_word = Input(shape=(CORP_SIZE,),name="word_layer_input")
+# input_context = Input(shape=(CORP_SIZE,),name="context_layer_input")
+#
+# word_layer = Dense(EMBED_SIZE, activation='linear', name="word_lay_den_lin")(input_word)
+# word_layer = Model(inputs = input_word, outputs=word_layer, name="word_layer_Model")
+#
+# context_layer = Dense(EMBED_SIZE, activation='linear', name="context_lay_den_lin")(input_context)
+# context_layer = Model(inputs = input_context, outputs=context_layer,name='context_layer_Model')
+#
+# combined = concatenate([word_layer.output, context_layer.output], name="concatenate")
+# output = Dense(1,activation='sigmoid',name="output_layer_1_sigmoid")(combined)
+#
+# model = Model(inputs = [word_layer.input,context_layer.input],outputs = output,name="final_model")
 
 # print(model.summary())
 # plot_model(model, to_file=mypath/"model.png")
+
+# opt = Adam(learning_rate=1e-3)
+#
+# model.compile(loss="binary_crossentropy",optimizer=opt,metrics=['accuracy'])
+#
+# model.fit(
+#     x=[X_word,X_context],
+#     y=y_train,
+#     epochs=200,
+#     batch_size=8
+# )
+
+
+
 
 # print(X_train[:10])
 # print(type(X_train)) # save vocabulary and pass it here ???
--- test2.py
+++ test2.py
@@ -1,47 +1,32 @@
-import torch
-from torchtext.legacy.data import Field, LabelField, TabularDataset, BucketIterator
-from torchtext.legacy.datasets import IMDB
-# from torchtext.datasets import IMDB
-import random
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# import spacy as sp
-# spacy_eng = sp.load('en')
+import numpy as np
+import tensorflow as tf
+X_train = np.array([
+                    [1,4],
+                    [3,2],
+                    [5,6],
+                    [3,4],
+                    [1,7],
+                    [5,10]
+                    ])
 
-TEXT = Field(tokenize='spacy', tokenizer_language='en_core_web_sm')
-LABEL = LabelField(dtype=torch.float)
+word = X_train[:,[0]].reshape(len(X_train),)
+context = X_train[:,[1]].reshape(len(X_train),)
 
+VOCAB_SIZE = 10
+X_word = np.zeros((len(X_train),11))
+for index, word_idx in enumerate(word):#X_train[:,[0]].reshape(6,)):
+    print(word_idx)
+    X_word[index][word_idx] = 1
 
-train, test = IMDB.splits(TEXT,LABEL)
-
-# print(len(train),len(test))
-# print(vars(train.examples[1])['text'])
-
-# for x in range(5):
-#     print(x,len(vars(train.examples[x])['text']),vars(train[x])['label'] ,vars(train.examples[x])['text'])
-
-
-#  splits provide only training / test -> below creates validation dataset (and shuffle?)
-train, validate = train.split(random_state = random.seed(1234), split_ratio = 0.8)
+print(X_word)
 
 
-# for x in range(5):
-#     print(x,len(vars(train.examples[x])['text']),vars(train[x])['label'] ,vars(train.examples[x])['text'])
 
-# create vocabulary
-
-TEXT.build_vocab(train)
-LABEL.build_vocab(test)
-
-print(TEXT.vocab)
-# print(TEXT.vocab.freqs.most_common(20))
-# print(TEXT.vocab.itos[:10])
-print(TEXT.vocab.stoi['undirected'])
-print(len(TEXT.vocab),len(LABEL.vocab))
-
-BATCH_SIZE = 64
+# X_word = []
 
 
-train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
-    (train, validate, test),
-    batch_size = BATCH_SIZE,
-    device = device)
+
+
+# print(X_train[:,[0]].reshape(6,))
+# print(tf.one_hot(X_train[:,[0]].reshape(6,),11))
+
