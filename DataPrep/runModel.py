41fb67e3e075b232811784567d13f21bd031f79b
Piotr JJ <error.replicator@gmail.com>
2022-06-06 16:22:16 +0200

    FIT_generator to address memory issues

--- DataPrep/runModel.py
+++ DataPrep/runModel.py
@@ -6,7 +6,8 @@ from keras.layers import Input,Dense, concatenate
 from keras.utils import plot_model
 from keras.optimizers import Adam
 import tensorflow as tf
-
+from sys import getsizeof
+from sklearn.utils import shuffle
 mypath = Path('/data/txtFiles/GutJustOne/')
 # mypath = Path('/data/txtFiles/GutSubset')
 # mypath = Path('/data/txtFiles/Gut1k')
@@ -19,10 +20,83 @@ with open(mypath/'X_Y_index_pairs.pklxy','rb') as file:
 with open(mypath/'tokenizer.pkltok','rb') as file:
     tokenizer = pickle.load(file)
 
-CORP_SIZE = 96000 #len(tokenizer.index_word)
-EMBED_SIZE = 50
+CORP_SIZE = len(tokenizer.index_word)+1
+EMBED_SIZE = 100
 
-X_word = np.zeros((CORP_SIZE, CORP_SIZE))
+# X_word = np.zeros((CORP_SIZE, EMBED_SIZE))
+# print(round(getsizeof(X_word) / 1024 / 1024,2), " MBs")
+
+def change_to_onehot(numbers_list,embed_size):
+    word_one_hot = np.zeros((1,embed_size))
+    context_one_hot = np.zeros((1, embed_size))
+    word_one_hot[0][numbers_list[0]] = 1
+    context_one_hot[0][numbers_list[1]] = 1
+    return (word_one_hot,context_one_hot)
+
+
+def data_generator (X_samples, y_samples, embed_size, batch_size=32, shuffle_data=True):
+    num_samples = len(X_samples)
+    while True:
+
+        if shuffle_data:
+            X_samples, y_samples = shuffle(X_samples, y_samples)
+
+
+
+        for offset in range(0,num_samples,batch_size): # take 1st 32(batch size) pairs
+
+            batch_X_samples = X_samples[offset:offset+batch_size]
+            batch_y_samples = y_samples[offset:offset+batch_size]
+
+            X_train_word = []
+            X_train_context = []
+            y_train = batch_y_samples
+
+            for sample in batch_X_samples:
+                X_word_vec, X_context_vec = change_to_onehot(sample,embed_size)
+
+                X_train_word.append(X_word_vec)
+                X_train_context.append(X_context_vec)
+
+            X_train_word = np.array(X_train_word)
+            X_train_context = np.array(X_train_context)
+            y_train = np.array(y_train)
+
+            yield X_train_word,X_train_context,y_train
+
+
+
+train_generator = data_generator(X_train,y_train,EMBED_SIZE)
+
+
+input_word = Input(shape=(CORP_SIZE,),name="word_layer_input")
+input_context = Input(shape=(CORP_SIZE,),name="context_layer_input")
+
+word_layer = Dense(EMBED_SIZE, activation='linear', name="word_lay_den_lin")(input_word)
+word_layer = Model(inputs = input_word, outputs=word_layer, name="word_layer_Model")
+
+context_layer = Dense(EMBED_SIZE, activation='linear', name="context_lay_den_lin")(input_context)
+context_layer = Model(inputs = input_context, outputs=context_layer,name='context_layer_Model')
+
+combined = concatenate([word_layer.output, context_layer.output], name="concatenate")
+output = Dense(1,activation='sigmoid',name="output_layer_1_sigmoid")(combined)
+
+model = Model(inputs = [word_layer.input,context_layer.input],outputs = output,name="final_model")
+
+# print(model.summary())
+# plot_model(model, to_file=mypath/"model.png")
+
+opt = Adam(learning_rate=1e-3)
+
+model.compile(loss="binary_crossentropy",optimizer=opt,metrics=['accuracy'])
+
+model.fit_generator(
+   train_generator,
+    epochs=200,
+    batch_size=8
+)
+
+
 
 
 #*****************************************
@@ -50,39 +124,6 @@ X_word = np.zeros((CORP_SIZE, CORP_SIZE))
 #
 # print(10*" STOP ")
 #*****************************************
-
-
-# input_word = Input(shape=(CORP_SIZE,),name="word_layer_input")
-# input_context = Input(shape=(CORP_SIZE,),name="context_layer_input")
-#
-# word_layer = Dense(EMBED_SIZE, activation='linear', name="word_lay_den_lin")(input_word)
-# word_layer = Model(inputs = input_word, outputs=word_layer, name="word_layer_Model")
-#
-# context_layer = Dense(EMBED_SIZE, activation='linear', name="context_lay_den_lin")(input_context)
-# context_layer = Model(inputs = input_context, outputs=context_layer,name='context_layer_Model')
-#
-# combined = concatenate([word_layer.output, context_layer.output], name="concatenate")
-# output = Dense(1,activation='sigmoid',name="output_layer_1_sigmoid")(combined)
-#
-# model = Model(inputs = [word_layer.input,context_layer.input],outputs = output,name="final_model")
-
-# print(model.summary())
-# plot_model(model, to_file=mypath/"model.png")
-
-# opt = Adam(learning_rate=1e-3)
-#
-# model.compile(loss="binary_crossentropy",optimizer=opt,metrics=['accuracy'])
-#
-# model.fit(
-#     x=[X_word,X_context],
-#     y=y_train,
-#     epochs=200,
-#     batch_size=8
-# )
-
-
-
-
 # print(X_train[:10])
 # print(type(X_train)) # save vocabulary and pass it here ???
 # print(len(tokenizer.index_word))
--- test.py
+++ test.py
@@ -1,38 +1,18 @@
 import numpy as np
-from sklearn.utils import shuffle
-import tensorflow as tf
-
-X_train = np.array([[1,4],[3,2],[5,6],[3,4],[1,7],[5,10]])
-y_train = np.array([22,1,2,3,4,15])
 
 
-for pair in X_train:
-    print(pair[0],tf.one_hot(pair[0],11))
-    print(pair[1],tf.one_hot(pair[1],11))
-    print(50*"*")
+word_one_hot = np.zeros((1,10))
+print(word_one_hot)
+word_one_hot[0][3] = 1
+print(word_one_hot)
+
+def change_to_onehot(numbers_list,embed_size):
+    word_one_hot = np.zeros((1,embed_size))
+    print(word_one_hot.shape)
+    context_one_hot = np.zeros((1, embed_size))
+    word_one_hot[0][numbers_list[0]] = 1
+    context_one_hot[0][numbers_list[1]] = 1
+    return (word_one_hot,context_one_hot)
 
 
-
-# X, y = shuffle(X_train,y_train)
-#
-# print(X)
-# print(y)
-
-
-
-# arr = [[1,2],[45,101],[22,222], [1,2]]
-#
-# arr = set(tuple(i) for i in arr)
-#
-# print(arr)
-#
-# arr = [list(i) for i in arr]
-# num_test = np.array([0,0])
-#
-# for element in test:
-#     print(element)
-#     if np.isin(num_test,element):
-#         pass
-#     else:
-#         np.append(num_test,element,axis=0)
-# print(num_test)
+print(change_to_onehot([2,7],10))
